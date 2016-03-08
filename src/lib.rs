fa799e5005fae4f7759924e71e0d524fe4c87962
Rory McCann <rory@technomancy.org>
2016-03-08 15:49:05 +0100

    feat: Add .fraction() method which tells you how far you are along

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ use time::{now_utc, Tm, Duration};
 pub struct ProgressRecord {
     num: usize,
     iterating_for: Duration,
+    size_hint: (usize, Option<usize>),
 
 }
 
@@ -41,7 +42,13 @@ impl ProgressRecord {
     /// None if we don't know how much we've done (as a fraction), otherwise a value form 0 to 1
     /// for what fraction along we are.
     pub fn fraction(&self) -> Option<f32> {
-        None
+        if self.is_size_known() {
+            let remaining = self.size_hint.0;
+            let done = self.num_done();
+            Some(( done as f32 ) / ( (remaining+done) as f32 ))
+        } else {
+            None
+        }
     }
 
     /// None if we don't know how much we've done, otherwise value for 0 to 100 representing how
@@ -62,8 +69,17 @@ impl ProgressRecord {
     /// Often you want to print out a debug message every 1,000 items or so. This function does
     /// that.
     pub fn print_every(&self, n: usize, msg: &str) {
-        if should_print_every_items(n) {
-            print!(msg);
+        if self.should_print_every_items(n) {
+            print!("{}", msg);
+        }
+    }
+
+    /// Does the size_hint tell us exactly how many items are left? False iff there is some
+    /// ambiguity/unknown
+    fn is_size_known(&self) -> bool {
+        match self.size_hint.1 {
+            None => { false },
+            Some(x) => { self.size_hint.0 == x },
         }
     }
 
@@ -75,7 +91,7 @@ pub struct ProgressRecorderIter<I> {
     started_iterating: Tm,
 }
 
-impl<I> ProgressRecorderIter<I> {
+impl<I: Iterator> ProgressRecorderIter<I> {
     /// Create a new `ProgressRecorderIter` from another iterator.
     pub fn new(iter: I) -> ProgressRecorderIter<I> {
         ProgressRecorderIter{ iter: iter, count: 0, started_iterating: now_utc() }
@@ -84,7 +100,7 @@ impl<I> ProgressRecorderIter<I> {
     /// Calculate the current `ProgressRecord` for where we are now.
     fn generate_record(&mut self) -> ProgressRecord {
         self.count += 1;
-        ProgressRecord{ num: self.count, iterating_for: now_utc() - self.started_iterating }
+        ProgressRecord{ num: self.count, iterating_for: now_utc() - self.started_iterating, size_hint: self.iter.size_hint() }
     }
 
 }
@@ -140,34 +156,59 @@ mod test {
         let (state, _) = progressor.next().unwrap();
         assert_eq!(state.message(), "Have seen 1 items and been iterating for 0");
         // It'll always print on the first one
-        assert_eq!(state.should_print_every(2), true);
-        assert_eq!(state.should_print_every(3), true);
-        assert_eq!(state.should_print_every(5), true);
+        assert_eq!(state.should_print_every_items(2), true);
+        assert_eq!(state.should_print_every_items(3), true);
+        assert_eq!(state.should_print_every_items(5), true);
         assert_eq!(state.rate(), ::std::f32::INFINITY);
 
         sleep_ms(500);
         let (state, _) = progressor.next().unwrap();
         assert_eq!(state.message(), "Have seen 2 items and been iterating for 1");
-        assert_eq!(state.should_print_every(2), false);
-        assert_eq!(state.should_print_every(3), false);
-        assert_eq!(state.should_print_every(5), false);
+        assert_eq!(state.should_print_every_items(2), false);
+        assert_eq!(state.should_print_every_items(3), false);
+        assert_eq!(state.should_print_every_items(5), false);
         assert_eq!(state.rate(), 2.);
 
         sleep_ms(500);
         let (state, _) = progressor.next().unwrap();
         assert_eq!(state.message(), "Have seen 3 items and been iterating for 1");
-        assert_eq!(state.should_print_every(2), true);
-        assert_eq!(state.should_print_every(3), false);
-        assert_eq!(state.should_print_every(5), false);
+        assert_eq!(state.should_print_every_items(2), true);
+        assert_eq!(state.should_print_every_items(3), false);
+        assert_eq!(state.should_print_every_items(5), false);
         assert_eq!(state.rate(), 3.);
 
         sleep_ms(500);
         let (state, _) = progressor.next().unwrap();
         assert_eq!(state.message(), "Have seen 4 items and been iterating for 2");
-        assert_eq!(state.should_print_every(2), false);
-        assert_eq!(state.should_print_every(3), true);
-        assert_eq!(state.should_print_every(5), false);
+        assert_eq!(state.should_print_every_items(2), false);
+        assert_eq!(state.should_print_every_items(3), true);
+        assert_eq!(state.should_print_every_items(5), false);
         assert_eq!(state.rate(), 2.);
+    }
+
+    #[test]
+    fn test_size_hint() {
+        use super::ProgressableIter;
+        use time::Duration;
+
+        let vec: Vec<u8> = vec![0, 1, 2, 3, 4];
+        let mut progressor = vec.iter().progress();
+
+        let (state, _) = progressor.next().unwrap();
+        assert_eq!(state.fraction(), Some(0.2));
+
+        let (state, _) = progressor.next().unwrap();
+        assert_eq!(state.fraction(), Some(0.4));
+
+        let mut progressor = (0..).progress();
+
+        let (state, val) = progressor.next().unwrap();
+        assert_eq!(val, 0);
+        assert_eq!(state.fraction(), None);
+        let (state, val) = progressor.next().unwrap();
+        assert_eq!(val, 1);
+        assert_eq!(state.fraction(), None);
+
     }
 }
 
