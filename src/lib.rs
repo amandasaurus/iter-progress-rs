b891b76edf1be7cd5e8e669e3c033a4804f28493
Rory McCann <rory@technomancy.org>
2019-09-28 10:56:41 +0200

    minor doc comment whitespace

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,6 @@ use std::time::{Instant, Duration};
 
 /// Every step of the underlying iterator, one of these is generated. It contains all the
 /// information of how this iterator is progresing. Use the methods to access data on it.
-///
 pub struct ProgressRecord {
 
     /// How many elements before this
